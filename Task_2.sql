8e3199297ef9d79f3cb1891786f89d922ee123a9
jaroslav.snajdar@seznam.cz <jaroslav.snajdar@snajdar.cz>
2023-10-30 19:41:17 +0100

    Zobrazení posledního srovnatelného obdhobí.

--- Task_2.sql
+++ Task_2.sql
@@ -33,3 +33,14 @@ SELECT
 	ROUND( wage_avg / SUM(average_price), 2) AS buy_times 
 FROM t_jaroslav_snajdar_task_2_first;
 
+SELECT 
+	payroll_year, 
+	name, 
+	average_price,
+	round(avg(average_gross_wage), 2) AS wage_avg
+FROM t_jaroslav_snajdar_project_SQL_primary_final tjspspf
+WHERE name IN ('Mléko polotučné pasterované', 'Chléb konzumní kmínový')
+GROUP BY payroll_year, name
+ORDER BY payroll_year DESC
+LIMIT 2;
+
