cc8c04a08fa3fcbe9809f7cfeb19fbe10efcd2d4
jaroslav.snajdar@seznam.cz <jaroslav.snajdar@snajdar.cz>
2023-10-30 19:57:41 +0100

    Vytvoření tabulky s cenami z prvního a posledního roku, jejich rozdíl v hodnotách a procentech.

--- Task_3.sql
+++ Task_3.sql
@@ -7,3 +7,13 @@ FROM t_jaroslav_snajdar_project_SQL_primary_final tjspspf
 WHERE name IS NOT NULL 
 GROUP BY payroll_year, name
 ORDER BY name, payroll_year; 
+
+CREATE OR REPLACE TABLE t_jaroslav_snajdar_task_3_2  
+SELECT 
+	*, 
+	LAG(average_price,1) OVER(PARTITION BY name ORDER BY payroll_year) AS previous,
+	average_price - LAG(average_price,1) OVER(PARTITION BY name ORDER BY payroll_year) AS different,
+	ROUND(((average_price - LAG(average_price,1) OVER(PARTITION BY name ORDER BY payroll_year)) / (LAG(average_price,1) OVER(PARTITION BY name ORDER BY payroll_year)) * 100), 2) AS different_percen
+FROM t_jaroslav_snajdar_task_3 tjst
+WHERE payroll_year IN ('2006', '2018')
+ORDER BY name, payroll_year;
