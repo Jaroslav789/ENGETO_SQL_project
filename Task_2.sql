a94ad75700fb13e88df9e4eeae0d30e452f1ec2a
jaroslav.snajdar@seznam.cz <jaroslav.snajdar@snajdar.cz>
2023-10-30 19:48:15 +0100

    Mzda vydělena cenami za chleba a mlíko v posledním sledovaném období.

--- Task_2.sql
+++ Task_2.sql
@@ -56,3 +56,9 @@ GROUP BY payroll_year, name
 ORDER BY payroll_year DESC 
 LIMIT 2;
 
+SELECT 
+	payroll_year,
+	wage_avg, 
+	SUM(average_price),
+	ROUND( wage_avg / SUM(average_price), 2) AS buy_times 
+FROM t_jaroslav_snajdar_task_2_last tjstl;
