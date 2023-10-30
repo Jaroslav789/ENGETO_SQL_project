0f48ba47f54bc68685c0351da4b6480040a3830a
jaroslav.snajdar@seznam.cz <jaroslav.snajdar@snajdar.cz>
2023-10-30 20:13:21 +0100

    Vytvoření tabulky s rozdílem procentním rustem cen a mezd.

---
+++ Task_4.sql
@@ -0,0 +1,16 @@
+SELECT 
+	payroll_year,
+	average_gross_wage,
+	industry_branch_name,
+	name,
+	average_price 
+FROM t_jaroslav_snajdar_project_SQL_primary_final tjspspf
+GROUP BY payroll_year;
+
+CREATE OR REPLACE TABLE t_jaroslav_snajdar_task_4
+SELECT 
+	payroll_year,
+	ROUND(((average_price - LAG(average_price,1) OVER(PARTITION BY name ORDER BY payroll_year)) / (LAG(average_price,1) OVER(PARTITION BY name ORDER BY payroll_year)) * 100), 2) - ROUND(((average_gross_wage - LAG(average_gross_wage,1) OVER(PARTITION BY name ORDER BY payroll_year)) / (LAG(average_gross_wage,1) OVER(PARTITION BY name ORDER BY payroll_year)) * 100), 2) AS price_wage_different
+FROM t_jaroslav_snajdar_project_SQL_primary_final tjspspf
+WHERE payroll_year BETWEEN 2006 AND 2018
+GROUP BY payroll_year;
