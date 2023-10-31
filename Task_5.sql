c818a4f4ba619c534363024cfa95cf28de5a6f1d
jaroslav.snajdar@seznam.cz <jaroslav.snajdar@snajdar.cz>
2023-10-31 19:55:24 +0100

    Vytvoření pomocné tabulky s procentními změnamí.

--- Task_5.sql
+++ Task_5.sql
@@ -1,17 +1,16 @@
+CREATE OR REPLACE TABLE t_jaroslav_snajdar_task_5
 SELECT 
+	tjspssf.country,
 	tjspssf.`year`,
-	 CASE
-        WHEN ROUND(((tjspssf.GDP - LAG(tjspssf.GDP,1) OVER(ORDER BY tjspssf.`year`)) / (LAG(tjspssf.GDP,1) OVER(ORDER BY tjspssf.`year`)) * 100), 2) > 5 THEN 'roste'
-        ELSE 'neroste'
-    END AS grow_GDP,
-     CASE
-        WHEN ROUND(((tjspspf.average_gross_wage - LAG(tjspspf.average_gross_wage,1) OVER(ORDER BY tjspssf.`year`)) / (LAG(tjspspf.average_gross_wage,1) OVER(ORDER BY tjspssf.`year`)) * 100), 2) > 5 THEN 'roste'
-        ELSE 'neroste'
-    END AS grow_PRICE, 
-    CASE
-        WHEN ROUND(((tjspspf.average_price - LAG(tjspspf.average_price,1) OVER(ORDER BY tjspssf.`year`)) / (LAG(tjspspf.average_price,1) OVER(ORDER BY tjspssf.`year`)) * 100), 2) > 5 THEN 'roste'
-        ELSE 'neroste'
-    END AS grow_WAGE
+	tjspssf.GDP,
+	tjspspf.average_gross_wage,
+	tjspspf.average_price, 
+	LAG(tjspssf.GDP,1) OVER(ORDER BY tjspssf.`year`) AS previous_GDP,
+	LAG(tjspspf.average_gross_wage,1) OVER(ORDER BY tjspssf.`year`) AS previous_WAGE,
+	LAG(tjspspf.average_price,1) OVER(ORDER BY tjspssf.`year`) AS previous_PRICE,
+	ROUND(((tjspssf.GDP - LAG(tjspssf.GDP,1) OVER(ORDER BY tjspssf.`year`)) / (LAG(tjspssf.GDP,1) OVER(ORDER BY tjspssf.`year`)) * 100), 2) AS different_GDP_percen,
+	ROUND(((tjspspf.average_gross_wage - LAG(tjspspf.average_gross_wage,1) OVER(ORDER BY tjspssf.`year`)) / (LAG(tjspspf.average_gross_wage,1) OVER(ORDER BY tjspssf.`year`)) * 100), 2) AS different_WAGE_percen,
+	ROUND(((tjspspf.average_price - LAG(tjspspf.average_price,1) OVER(ORDER BY tjspssf.`year`)) / (LAG(tjspspf.average_price,1) OVER(ORDER BY tjspssf.`year`)) * 100), 2) AS different_PRICE_percen
 FROM t_jaroslav_snajdar_project_SQL_secondary_final tjspssf
 LEFT JOIN t_jaroslav_snajdar_project_SQL_primary_final tjspspf 
 	 ON tjspspf.payroll_year = tjspssf.`year` 
