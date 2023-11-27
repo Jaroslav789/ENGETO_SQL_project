6678b1cfa69727ce637b1a7ab83c94d3d9ed7903
jaroslav.snajdar@seznam.cz <jaroslav.snajdar@snajdar.cz>
2023-11-27 19:57:38 +0100

    Zalomení řádku 13

--- Task_4.sql
+++ Task_4.sql
@@ -10,7 +10,8 @@ GROUP BY payroll_year;
 CREATE OR REPLACE TABLE t_jaroslav_snajdar_task_4
 SELECT 
 	payroll_year,
-	ROUND(((average_price - LAG(average_price,1) OVER(PARTITION BY name ORDER BY payroll_year)) / (LAG(average_price,1) OVER(PARTITION BY name ORDER BY payroll_year)) * 100), 2) - ROUND(((average_gross_wage - LAG(average_gross_wage,1) OVER(PARTITION BY name ORDER BY payroll_year)) / (LAG(average_gross_wage,1) OVER(PARTITION BY name ORDER BY payroll_year)) * 100), 2) AS price_wage_different
+	ROUND(((average_price - LAG(average_price,1) OVER(PARTITION BY name ORDER BY payroll_year)) / (LAG(average_price,1) OVER(PARTITION BY name ORDER BY payroll_year)) * 100), 2)
+	- ROUND(((average_gross_wage - LAG(average_gross_wage,1) OVER(PARTITION BY name ORDER BY payroll_year)) / (LAG(average_gross_wage,1) OVER(PARTITION BY name ORDER BY payroll_year)) * 100), 2) AS price_wage_different
 FROM t_jaroslav_snajdar_project_SQL_primary_final tjspspf
 WHERE average_gross_wage IS NOT NULL 
 GROUP BY payroll_year;
