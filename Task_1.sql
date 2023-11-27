144149cc77b8ee442d18c5b10dc13f0e3963bb22
jaroslav.snajdar@seznam.cz <jaroslav.snajdar@snajdar.cz>
2023-11-27 19:32:18 +0100

    přirání where is not null u average_gross_wage a industry_branch_name

--- Task_1.sql
+++ Task_1.sql
@@ -1,9 +1,14 @@
+CREATE OR REPLACE TABLE t_jaroslav_snajdar_task_1_1
+SELECT *
+FROM t_jaroslav_snajdar_project_SQL_primary_final tjspspf
+WHERE average_gross_wage IS NOT NULL;
+
 CREATE OR REPLACE TABLE t_jaroslav_snajdar_task_1
 SELECT 
 	payroll_year,
 	industry_branch_name,
 	average_gross_wage
-FROM t_jaroslav_snajdar_project_SQL_primary_final tjspspf
+FROM t_jaroslav_snajdar_task_1_1
 WHERE payroll_year = '2000' OR payroll_year = '2021'
 GROUP BY industry_branch_name, payroll_year;
 
@@ -14,6 +19,7 @@ SELECT
 	payroll_year,
 	LAG(average_gross_wage) OVER(ORDER BY industry_branch_name) AS previous_AVG_wage
 FROM t_jaroslav_snajdar_task_1 tjst
+WHERE industry_branch_name IS NOT NULL
 GROUP BY industry_branch_name, payroll_year;
 
 SELECT 
