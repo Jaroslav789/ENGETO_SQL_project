1808772eba80fa63d5e7262c20b02480fc4577a9
Jaroslav789 <145101537+Jaroslav789@users.noreply.github.com>
2023-11-30 21:39:34 +0100

    Update Task_5.sql
    Zjednodušení scriptu, úprava nazvu.

--- Task_5.sql
+++ Task_5.sql
@@ -2,15 +2,9 @@ CREATE OR REPLACE TABLE t_jaroslav_snajdar_task_5
 SELECT 
 	tjspssf.country,
 	tjspssf.`year`,
-	tjspssf.GDP,
-	tjspspf.average_gross_wage,
-	tjspspf.average_price, 
-	LAG(tjspssf.GDP,1) OVER(ORDER BY tjspssf.`year`) AS previous_GDP,
-	LAG(tjspspf.average_gross_wage,1) OVER(ORDER BY tjspssf.`year`) AS previous_WAGE,
-	LAG(tjspspf.average_price,1) OVER(ORDER BY tjspssf.`year`) AS previous_PRICE,
-	ROUND(((tjspssf.GDP - LAG(tjspssf.GDP,1) OVER(ORDER BY tjspssf.`year`)) / (LAG(tjspssf.GDP,1) OVER(ORDER BY tjspssf.`year`)) * 100), 2) AS different_GDP_percen,
-	ROUND(((tjspspf.average_gross_wage - LAG(tjspspf.average_gross_wage,1) OVER(ORDER BY tjspssf.`year`)) / (LAG(tjspspf.average_gross_wage,1) OVER(ORDER BY tjspssf.`year`)) * 100), 2) AS different_WAGE_percen,
-	ROUND(((tjspspf.average_price - LAG(tjspspf.average_price,1) OVER(ORDER BY tjspssf.`year`)) / (LAG(tjspspf.average_price,1) OVER(ORDER BY tjspssf.`year`)) * 100), 2) AS different_PRICE_percen
+	ROUND(AVG(tjspssf.GDP), 0) AS gdp,
+	ROUND(AVG(tjspspf.average_gross_wage), 0) AS wage,
+	ROUND(AVG(tjspspf.price), 2) AS price
 FROM t_jaroslav_snajdar_project_SQL_secondary_final tjspssf
 LEFT JOIN t_jaroslav_snajdar_project_SQL_primary_final tjspspf 
 	 ON tjspspf.payroll_year = tjspssf.`year` 
@@ -18,33 +12,34 @@ WHERE average_gross_wage IS NOT NULL AND country = 'Czech Republic'
 GROUP BY tjspssf.`year`
 ORDER BY tjspssf.`year`;
 
-SELECT 
-	`year`,
-	gdp,
-	average_gross_wage AS wage,
-	average_price AS price,
-	different_GDP_percen AS GDP_p,
-	different_WAGE_percen AS WAGE_p,
-	different_PRICE_percen AS PRICE_p
-FROM t_jaroslav_snajdar_task_5 tjst;
+SELECT *
+FROM t_jaroslav_snajdar_task_5;
 
+WITH a AS (
+SELECT 
+	*,
+	ROUND(((gdp - LAG(gdp,1) OVER(ORDER BY `year`)) / (LAG(gdp,1) OVER(ORDER BY `year`)) * 100), 2) AS d_gbp_percen,
+	ROUND(((wage - LAG(wage,1) OVER(ORDER BY `year`)) / (LAG(wage,1) OVER(ORDER BY `year`)) * 100), 2) AS d_ware_percen,
+	ROUND(((price - LAG(price,1) OVER(ORDER BY `year`)) / (LAG(price,1) OVER(ORDER BY `year`)) * 100), 2) AS d_rage_percen
+FROM t_jaroslav_snajdar_task_5 tjst
+)
 SELECT 
 	`year`,
 	CASE
-        WHEN  different_GDP_percen > 2 AND different_WAGE_percen > 2 THEN 'roste'
+        WHEN  d_gbp_percen > 2 AND d_ware_percen > 2 THEN 'roste'
         ELSE ''
     END AS wage_this_year,
     CASE
-        WHEN  different_GDP_percen > 2 AND different_PRICE_percen > 2 THEN 'roste'
+        WHEN  d_gbp_percen > 2 AND d_rage_percen > 2 THEN 'roste'
         ELSE ''
     END AS price_this_year,
 	CASE 
-    	WHEN different_GDP_percen > 2 AND LAG(tjst.different_WAGE_percen,-1) OVER(ORDER BY tjst.`year`) > 2 THEN 'roste'
+    	WHEN d_gbp_percen > 2 AND LAG(d_ware_percen,-1) OVER(ORDER BY `year`) > 2 THEN 'roste'
     	ELSE ''
     END AS wage_next_year,
     CASE 
-    	WHEN different_GDP_percen > 2 AND LAG(tjst.different_PRICE_percen,-1) OVER(ORDER BY tjst.`year`) > 2 THEN 'roste'
+    	WHEN d_gbp_percen > 2 AND LAG(d_rage_percen,-1) OVER(ORDER BY `year`) > 2 THEN 'roste'
     	ELSE ''
     END AS price_next_year
-FROM t_jaroslav_snajdar_task_5 tjst
-WHERE different_GDP_percen IS NOT NULL; 
+FROM a
+WHERE d_gbp_percen IS NOT NULL; 
